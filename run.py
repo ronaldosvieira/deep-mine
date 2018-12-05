4a27c831ea23bf074c44c2978690cef00f6b48c1
Ronaldo Vieira <ronaldoesv@gmail.com>
2018-12-05 12:19:44 -0200

    Keeps training until episode ends

--- run.py
+++ run.py
@@ -21,7 +21,6 @@ alpha = 0.0002 # learning rate
 
 # training hyperparameters
 total_episodes = 1
-#max_steps = 100
 batch_size = 64
 
 # exp-exp parameters for epsilon-greedy
@@ -190,6 +189,8 @@ def run_episode(sess, episode):
     # Enter game loop
     done = False
 
+    print("Pre-training...")
+
     # pre train
     for i in range(pretrain_length):
         # If it's the first step
@@ -227,78 +228,79 @@ def run_episode(sess, episode):
 
     state, stacked_frames = stack_frames(stacked_frames, observation, True)
 
-    step += 1
-    decay_step += 1
+    print("Starting training")
 
-    action, explore_probability = predict_action(explore_start, 
-        explore_stop, decay_rate, decay_step, state, sess)
+    while True:
+        step += 1
+        decay_step += 1
 
-    print(action)
-    observation, reward, done, info = env.step(action)
+        action, explore_probability = predict_action(explore_start, 
+            explore_stop, decay_rate, decay_step, state, sess)
 
-    episode_rewards.append(reward)
+        observation, reward, done, info = env.step(action)
 
-    if done:
-        env.close()
+        episode_rewards.append(reward)
 
-        return
-    else:
-        next_state, stacked_frames = stack_frames(stacked_frames,
-            observation, False)
+        if done:
+            env.close()
 
-        memory.add((state, action, reward, next_state, done))
-
-        state = next_state
-
-    ### LEARNING PART
-    def ac(a):
-        r = [0] * len(possible_actions)
-        r[a] = 1
-        return r            
-    # Obtain random mini-batch from memory
-    batch = memory.sample(batch_size)
-    states_mb = np.array([each[0] for each in batch], ndmin=3)
-    actions_mb = np.array([ac(each[1]) for each in batch])
-    rewards_mb = np.array([each[2] for each in batch]) 
-    next_states_mb = np.array([each[3] for each in batch], ndmin=3)
-    dones_mb = np.array([each[4] for each in batch])
-
-    target_Qs_batch = []
-
-     # Get Q values for next_state 
-    Qs_next_state = sess.run(DQNetwork.output, 
-        feed_dict = {DQNetwork.inputs_: next_states_mb})
-
-    # Set Q_target = r if the episode ends at s+1, 
-    # otherwise set Q_target = r + gamma*maxQ(s', a')
-    for i in range(0, len(batch)):
-        terminal = dones_mb[i]
-
-        # If we are in a terminal state, only equals reward
-        if terminal:
-            target_Qs_batch.append(rewards_mb[i])
-            
+            return
         else:
-            target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
-            target_Qs_batch.append(target)
-            
+            next_state, stacked_frames = stack_frames(stacked_frames,
+                observation, False)
 
-    targets_mb = np.array([each for each in target_Qs_batch])
+            memory.add((state, action, reward, next_state, done))
 
-    loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
-                        feed_dict={DQNetwork.inputs_: states_mb,
-                                   DQNetwork.target_Q: targets_mb,
-                                   DQNetwork.actions_: actions_mb})
+            state = next_state
 
-    # Write TF Summaries
-    summary = sess.run(write_op, feed_dict={DQNetwork.inputs_: states_mb,
+        ### LEARNING PART
+        def ac(a):
+            r = [0] * len(possible_actions)
+            r[a] = 1
+            return r            
+        # Obtain random mini-batch from memory
+        batch = memory.sample(batch_size)
+        states_mb = np.array([each[0] for each in batch], ndmin=3)
+        actions_mb = np.array([ac(each[1]) for each in batch])
+        rewards_mb = np.array([each[2] for each in batch]) 
+        next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+        dones_mb = np.array([each[4] for each in batch])
+
+        target_Qs_batch = []
+
+         # Get Q values for next_state 
+        Qs_next_state = sess.run(DQNetwork.output, 
+            feed_dict = {DQNetwork.inputs_: next_states_mb})
+
+        # Set Q_target = r if the episode ends at s+1, 
+        # otherwise set Q_target = r + gamma*maxQ(s', a')
+        for i in range(0, len(batch)):
+            terminal = dones_mb[i]
+
+            # If we are in a terminal state, only equals reward
+            if terminal:
+                target_Qs_batch.append(rewards_mb[i])
+                
+            else:
+                target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+                target_Qs_batch.append(target)
+                
+
+        targets_mb = np.array([each for each in target_Qs_batch])
+
+        loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
+                            feed_dict={DQNetwork.inputs_: states_mb,
                                        DQNetwork.target_Q: targets_mb,
                                        DQNetwork.actions_: actions_mb})
-    writer.add_summary(summary, episode)
-    writer.flush()
 
-    save_path = saver.save(sess, "./models/model.ckpt")
-    print("Model Saved")
+        # Write TF Summaries
+        summary = sess.run(write_op, feed_dict={DQNetwork.inputs_: states_mb,
+                                           DQNetwork.target_Q: targets_mb,
+                                           DQNetwork.actions_: actions_mb})
+        writer.add_summary(summary, episode)
+        writer.flush()
+
+    print("Sum of rewards:", sum(episode_rewards))
 
     # It is important to do this env.close()
     env.close()
@@ -330,7 +332,13 @@ if __name__ == "__main__":
             sess.run(tf.global_variables_initializer())
 
             for episode in range(total_episodes):
+                print("Episode {}".format(episode))
+
                 run_episode(sess, episode)
+
+                if (episode + 1) % 5 == 0:
+                    save_path = saver.save(sess, "./models/model.ckpt")
+                    print("Model Saved")
         else:
             while True:
                 run_episode(sess, episode)
